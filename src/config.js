f2634ce0304c8d911ea56b65d76ea7a990e4fb0e
biancavee15 <vasquezbmarie@gmail.com>
2018-04-20 15:38:03 -0400

    client side works!

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL = 
-process.env.REACT_APP_API_BASE_URL || 'https://git.heroku.com/bianca-cheese-list.git'
+process.env.REACT_APP_API_BASE_URL || 'https://bianca-cheese-list.herokuapp.com/'
